1c6884615d3230a497763e1c673abfb6e1f13ccc
simon <simonireilly@gmail.com>
2018-01-24 19:22:02 +0000

    Implementd skills with a model generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Skill.create!(title: 'Ruby on Rails', percent_utilized: 60)
+Skill.create!(title: 'HTML', percent_utilized: 15)
+Skill.create!(title: 'Javascript', percent_utilized: 10)
+Skill.create!(title: 'scss /css', percent_utilized: 5)

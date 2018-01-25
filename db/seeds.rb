5432aaf5c01ee64d623090f4666049db807b1331
simon <simonireilly@gmail.com>
2018-01-25 06:24:26 +0000

    Created dynamic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,31 @@
-Skill.create!(title: 'Ruby on Rails', percent_utilized: 60)
-Skill.create!(title: 'HTML', percent_utilized: 15)
-Skill.create!(title: 'Javascript', percent_utilized: 10)
-Skill.create!(title: 'scss /css', percent_utilized: 5)
+10.times do |blog|
+  Blog.create!(
+    title: "My blog posts #{blog}",
+    body: (:lorem_ipsum).words(100)
+  )
+end
+
+puts '10 blog posts created'
+
+5.times do |blog|
+  Skill.create!(
+    title: "Rails #{blog}",
+    percent_utilized: 15
+  )
+end
+
+puts '5 skills created'
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Rails #{portfolio_item}",
+    subtitle: "Rails portfolio item subtitle #{portfolio_item}",
+    body: (:lorem_ipsum).words(100),
+    main_image: 'https://picsum.photos/600/400',
+    thumb_image: 'https://picsum.photos/350/200'
+  )
+end
+
+
+
+puts '9 Portfolio items created'

f39f78448b04407970378a361fcd456ee697685b
simon <simonireilly@gmail.com>
2018-01-26 06:42:04 +0000

    Making static page routes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,6 +37,14 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Record was removed.' }
+    end
+  end
+
   private
 
   def portfolio_params

01da6955f26fd968629e870f58b34a34a7373512
simon <simonireilly@gmail.com>
2018-02-05 07:17:49 +0000

    Added a custom scope, ontroller method, view and route

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,43 @@
+3.times do |topic|
+  Topic.create!(title: "Topic #{topic}")
+end
+
 10.times do |blog|
   Blog.create!(
     title: "My blog posts #{blog}",
-    body: Forgery(:lorem_ipsum).words(100)
+    body: Forgery(:lorem_ipsum).words(100),
+    topic_id: Topic.last.id
   )
 end
 
 puts '10 blog posts created'
 
-5.times do |blog|
+5.times do |skill|
   Skill.create!(
-    title: "Rails #{blog}",
+    title: "Rails #{skill}",
     percent_utilized: 15
   )
 end
 
 puts '5 skills created'
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Rails #{portfolio_item}",
-    subtitle: "Rails portfolio item subtitle #{portfolio_item}",
+    subtitle: "Ruby on Rails",
     body: Forgery(:lorem_ipsum).words(100),
     main_image: 'https://picsum.photos/600/400',
     thumb_image: 'https://picsum.photos/350/200'
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+      title: "Rails #{portfolio_item}",
+      subtitle: "Angular",
+      body: Forgery(:lorem_ipsum).words(100),
+      main_image: 'https://picsum.photos/600/400',
+      thumb_image: 'https://picsum.photos/350/200'
   )
 end
 

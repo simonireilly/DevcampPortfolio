361c4ee7e8a59dfbe818507ad9c20a1eb1be24a4
simon <simonireilly@gmail.com>
2018-02-28 07:32:36 +0000

    Added petergate for authorisation and implemented a site_admin profile

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,9 +6,11 @@ module CurrentUserConcern
   end
 
   def guest_user
-    OpenStruct.new(name: 'Guest user',
-                   first_name: 'Guest',
-                   last_name: 'User',
-                   email: 'test@test.com')
+    guest = GuestUser.new
+    guest.name = "Guest User"
+    guest.first_name = "Guest"
+    guest.last_name = "User"
+    guest.email = "guest@example.com"
+    return guest
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,12 @@ module ApplicationHelper
   end
 
   def login_helper
-    if current_user.is_a?(User)
-      link_to 'log out', destroy_user_session_path, method: :delete
-    else
+    if current_user.is_a?(GuestUser)
       (link_to 'Log in', new_user_session_path) +
-      content_tag(:br) +
-      (link_to 'Register', new_user_registration_path)
+        content_tag(:br) +
+        (link_to 'Register', new_user_registration_path)
+    else
+      link_to 'log out', destroy_user_session_path, method: :delete
     end
   end
 

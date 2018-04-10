5e60655e313f9873a8c81abccb76dbe6f00c3ed4
simonireilly <simonireilly@gmail.com>
2018-04-10 07:25:54 +0100

    Added styling to comment on blog view

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,5 @@
 class Topic < ApplicationRecord
   has_many :blogs
-  validates_presence_of :title
+  validates_presence_of :title, unique: true,
+                        length: {minimum: 2, maximum: 25}
 end

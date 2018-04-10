48f0e00c2c467ea322110ffcafb64300cae2a6f4
simonireilly <simonireilly@gmail.com>
2018-04-10 07:34:39 +0100

    Added validations on title, required uniqueness, minium and maximum length

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ApplicationRecord
   has_many :blogs
-  validates_presence_of :title, unique: true,
-                        length: {minimum: 2, maximum: 25}
+  validates :title, presence: true,
+                    uniqueness: true,
+                    length: { minimum: 2, maximum: 25 }
 end
